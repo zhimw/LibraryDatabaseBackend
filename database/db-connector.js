56225484cea005b26dedc72fa312ce8cb828e4ea
Zhimei Wang <happywzm@gmail.com>
2024-01-30 13:01:18 -0500

    modified db connection

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,16 +1,31 @@
-// ./database/db-connector.js
+require("dotenv").config();
 
 // Get an instance of mysql we can use in the app
-var mysql = require('mysql')
+var mysql = require("mysql2");
+
+const dbConn = mysql.createConnection({
+    connectionLimit: process.env.connectionLimit,
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+});
+dbConn.connect(err => {
+    if (err) throw err;
+    console.log('Connected!');
+});
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : '',
-    user            : '',
-    password        : '',
-    database        : ''
-})
+  connectionLimit: process.env.connectionLimit,
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+});
+
 
 // Export it for use in our applicaiton
 module.exports.pool = pool;
